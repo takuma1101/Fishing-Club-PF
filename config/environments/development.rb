c6ba796c00e0d3f1b3daa562972fdf5d6b01c0ca
takuma1101 <mtnr.baseball0821@gmail.com>
2022-10-13 14:32:29 +0000

    ログイン機能作成　終了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "14f26b1504e243d7ac8db0f82b671534.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+
+Admin.create!(
+   email: 'sakuragi@love',
+   password: 'dannku'
+)# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
